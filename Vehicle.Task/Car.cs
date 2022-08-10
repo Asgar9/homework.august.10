817d8e67634dbe0d01c235a0ef4604399a2f3e54
Asgar9 <asgaryas@code.edu.az>
2022-08-10 12:01:10 +0400

    Vehicle task

---
+++ Vehicle.Task/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicle.Task
+{
+    class Car:Vehicle
+    {
+        public string Brend;
+        public string Model;
+        public int FuelCapacity;
+        public int FuelFor1Km;
+        public int CurrentFuel;
+        public Car(string Brend,string Color, string Model,int Year, int FuelCapacity,int FuelFor1Km,int CurrentFuel)
+        {
+            this.Brend = Brend;
+            this.Color = Color;
+            this.Model = Model;
+            this.Year = Year;
+            this.FuelCapacity = FuelCapacity;
+            this.FuelFor1Km = FuelFor1Km;
+            this.CurrentFuel = CurrentFuel;
+            
+                
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Brend} {Color} {Model} {Year} {FuelCapacity} {FuelFor1Km} {CurrentFuel} ");
+        }
+        public int Drive(int way)
+        {    
+            CurrentFuel = FuelCapacity - (FuelFor1Km * way);
+            return CurrentFuel;
+        }
+    }
+}
---
+++ Vehicle.Task/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vehicle.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car("bmw", "black", "X5", 2011, 50, 3, 40);
+            bmw.ShowInfo();
+            bmw.Drive(5);
+        }
+    }
+}
